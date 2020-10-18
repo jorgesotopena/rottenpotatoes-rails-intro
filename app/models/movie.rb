708fc41ea1a744cbee307c7afad465516e2f9c55
Jorge Soto Pena <jorgesotopena@berkeley.edu>
2020-10-18 06:42:37 +0000

    part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings()
+    ['G', 'PG', 'PG-13', 'R']
+  end
+  
+  def self.with_ratings(ratings)
+    if ratings.empty?
+      Movie.all
+    else
+      Movie.where(rating: ratings)
+    end
+  end
 end
